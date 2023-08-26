b0a97871c730eae0c7aed4b6a63fb8ded4a31c7c
avishesh karki <avishesh09401kr@gmail.com>
2023-08-26 12:52:04 +0545

    Changed base url to enviroment var key

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,7 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8080"
+    : "https://taskoo.onrender.com/";
 const USER_URL = "/user";
 const TASK_URL = "/task";
 
